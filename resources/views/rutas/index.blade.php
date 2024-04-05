a8bb94379906dfc58586953b3768e5f356b0cf0d
Koopa2112 <127332126+Koopa2112@users.noreply.github.com>
2024-04-05 11:09:43 -0600

    Indice de rutas
    Alineación de texto

--- resources/views/rutas/index.blade.php
+++ resources/views/rutas/index.blade.php
@@ -11,11 +11,11 @@
 
             <thead>
                 <tr>
-                    <th>ID ruta</th>
+                    <th style="text-align: center;">ID ruta</th>
                     <th>Dia</th>
                     <th>Zona</th>
-                    <th>Max</th>
-                    <th>Programadas</th>
+                    <th style="text-align: center;">Max</th>
+                    <th style="text-align: center;">Programadas</th>
                     <th></th>
                 </tr>
             </thead>
@@ -25,11 +25,11 @@
                 @foreach($rutas as $ruta)
                 <tr>
 
-                    <th>{{ $ruta->id}}</th>
+                    <th style="text-align: center;">{{ $ruta->id}}</th>
                     <th>{{ $ruta->fecha_entrega }}</th>
                     <th>{{ $ruta->zona->nombre_zona }}</th>
-                    <th>{{ $ruta->max_entregas}}</th>
-                    <th>{{ $ruta->num_entregas}}</th>
+                    <th style="text-align: center;">{{ $ruta->max_entregas}}</th>
+                    <th style="text-align: center;">{{ $ruta->num_entregas}}</th>
 
                     <th><a href="{{  url('rutas/' .$ruta->id. '/edit') }}"
                             class="btn btn-primary btn-small">Editar ruta</a></th>
