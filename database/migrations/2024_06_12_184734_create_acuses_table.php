c97592f0ee7061165449875a53c40a30cbbc11dd
Koopa2112 <127332126+Koopa2112@users.noreply.github.com>
2024-07-22 11:46:51 -0600

    Update 2024_06_12_184734_create_acuses_table.php
    Actualización de columnas de acuses

--- database/migrations/2024_06_12_184734_create_acuses_table.php
+++ database/migrations/2024_06_12_184734_create_acuses_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('acuses', function (Blueprint $table) {
             $table->id();
-            $table->date('fecha_acuse');
-            $table->timestamp('fecha');
+            $table->date('fecha_creado');
+            $table->date('fecha_sellado');
+            $table->boolean('cerrado');
             $table->timestamps();
         });
     }
