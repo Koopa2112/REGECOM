409dc00be2d1ac35080d07e87982e9e37184d7a8
Koopa2112 <127332126+Koopa2112@users.noreply.github.com>
2024-08-26 16:30:59 -0600

    Update supervisor.blade.php
    Botones para acceder a la información básica de las rutas

--- resources/views/inicios/supervisor.blade.php
+++ resources/views/inicios/supervisor.blade.php
@@ -28,8 +28,15 @@
     </ul>
 </li>
 
+<li class="nav-item dropdown">
+    <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        Rutas
+    </button>
+    <ul class="dropdown-menu dropdown-menu-dark">
+        <li><a class="dropdown-item" href="{{ url('rutas') }}">Ver rutas</a></li>
 
-
+    </ul>
+</li>
 
 
 @stop
