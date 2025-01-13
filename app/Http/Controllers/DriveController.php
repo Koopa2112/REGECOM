c9b22c705cbd36567496111978b36a5d1856a150
Koopa2112 <127332126+Koopa2112@users.noreply.github.com>
2025-01-13 17:28:10 -0600

    Update DriveController.php
    descarga de archivo

--- app/Http/Controllers/DriveController.php
+++ app/Http/Controllers/DriveController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Google\Exception;
+use Google\Service\Drive;
+use PhpOffice\PhpWord\PhpWord;
+use PhpOffice\PhpWord\IOFactory;
 
 class DriveController extends Controller
 {
@@ -62,6 +65,61 @@ class DriveController extends Controller
             return $m->error->message;
         } catch (Exception $e) {
             return $e->getMessage();
+        }
+    }
+
+    public function descargaINE($linea)
+    {
+        $client = new \Google_Client();
+        $client->setAuthConfig(storage_path('app\credentials\storageid-07a73687097e.json')); // Ruta a tus credenciales
+        $client->addScope(Drive::DRIVE_READONLY);
+
+        $service = new Drive($client);
+        //$service = new Drive($client);
+        $files = $service->files->listFiles([
+            'q' => "(name='{$linea}_anverso_INE' or name='{$linea}_reverso_INE') and trashed=false",
+            'fields' => 'files(id, name)'
+        ]);
+
+        $fileList = $files->getFiles();
+
+        if (count($fileList) > 0) {
+            $phpWord = new PhpWord();
+            $section = $phpWord->addSection();
+
+            foreach ($fileList as $file) {
+                $fileId = $file->getId();
+                $fileName = $file->getName();
+
+                $response = $service->files->get($fileId, ['alt' => 'media']);
+                $content = $response->getBody()->getContents();
+
+                // Guardar la imagen temporalmente
+                $tempFilePath = storage_path('temp\ ' . $fileName . '.png');
+                file_put_contents($tempFilePath, $content);
+
+                // Añadir la imagen al documento
+                $section->addImage($tempFilePath, [
+                    'width' => 486,  // Ajusta el tamaño de la imagen según sea necesario
+                    'height' => 291,
+                ]);
+
+                
+            }
+            // Guardar el archivo .docx
+            $path = $linea . "identificación.docx";
+            $docxFilePath = storage_path($path);
+            $objWriter = IOFactory::createWriter($phpWord, 'Word2007');
+            $objWriter->save($docxFilePath);
+            foreach ($fileList as $file) {
+                $fileName = $file->getName();
+                $tempFilePath = storage_path('temp\ ' . $fileName . '.png');
+                unlink($tempFilePath);
+            }
+            // Descargar el archivo .docx
+            return response()->download($docxFilePath)->deleteFileAfterSend(true);
+        } else {
+            return response()->json(['error' => 'Files not found'], 404);
         }
     }
 }
